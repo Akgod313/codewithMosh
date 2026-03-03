d185c633f2a247e3048e7da902f0c8df8b470d20
Ayaan Karkera <ayaankarkera8@gmail.com>
2026-03-03 23:06:37 +0530

    Save 1

---
+++ codewithmoshtest/app/components/Productcart.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+import styles from "./Productcart.module.css"
+
+const Productcart = () => {
+  return (
+    <div>
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default Productcart
