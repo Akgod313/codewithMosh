052ee9b71f836e3a7397c23db33faed4e38855f2
Ayaan Karkera <ayaankarkera8@gmail.com>
2026-03-05 20:52:42 +0530

    second commit

---
+++ codewithmoshtest/app/users/UserTable.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+interface User {
+  id : number;
+  name : string;
+}
+const UserTable = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
+    const users: User[] = await res.json();
+    return (
+    <table>
+        <p>{new Date().toLocaleTimeString()}</p>
+        <ul>
+            {users.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>
+    </table>
+      
+  )
+}
+
+export default UserTable
